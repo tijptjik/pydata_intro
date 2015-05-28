40786f7daa8a379b025d1cc4af6a96b159acac8f
Mart van de Ven <m@type.hk>
2015-05-28 18:40:26 +0800

    new

--- theme/custom.js
+++ theme/custom.js
@@ -2,13 +2,13 @@
 
 %%javascript
 
-window.load_remote_theme = true
+window.load_remote_theme = false;
 var theme_url = "https://drostehk.github.io/ipynb-theme/";
 var asset_url = 'https://raw.githubusercontent.com/tijptjik/DS_assets/master/';
 
 window.load_local_theme = function(){
-    var hostname = document.location.hostname
-    return ((hostname == "localhost" || hostname == '127.0.0.1') && !load_remote_theme)
+    var hostname = document.location.hostname;
+    return ((hostname == "localhost" || hostname == '127.0.0.1') && !load_remote_theme);
 }
 
 var url = load_local_theme() ? document.location.origin + "/files/theme/custom.js" : theme_url + 'custom.js'
@@ -17,6 +17,8 @@ $.getScript(url)
 
 
 */
+
+console.log('hello')
 
 // Theme and Asset base URLs, change these to your fork.
 
@@ -98,3 +100,68 @@ $('.rendered_html').on('show', function() {
 })
 
 $('.rendered_html').trigger('show')
+
+// Gist Integration
+
+IPython.ext_update_gist_link = function(gist_id) {
+
+    IPython.notebook.metadata.gist_id = gist_id;
+    var toolbar = IPython.toolbar.element;
+    var link = toolbar.find("a#nbviewer");
+    if ( ! link.length ) {
+        link = $('<a id="nbviewer" target="_blank"/>');
+        toolbar.append(
+            $('<span id="nbviewer_span"/>').append(link)
+        );
+    }
+
+    link.attr("href", "http://nbviewer.ipython.org/" + gist_id);
+    link.text("http://nbviewer.ipython.org/" + gist_id);
+};
+
+IPython.ext_handle_gist_output = function(output_type, content) {
+    console.log(output_type)
+    console.log(content)
+    alert("Gist is loaded: ");
+    if (output_type != 'stream' || content['name'] != 'stdout') {
+        return;
+    }
+    var gist_id = jQuery.trim(content['data']);
+    if (! gist_id.match(/[A-Za-z0-9]+/g)) {
+        alert("Gist seems to have failed: " + gist_id);
+        return;
+    }
+    IPython.ext_update_gist_link(gist_id);
+};
+
+IPython.ext_gist_notebook = function () {
+    var gist_id = IPython.notebook.metadata.gist_id || null;
+    var cmd = '_nbname = "' + IPython.notebook.notebook_name + '"';
+    cmd = cmd + '\nlines = !gist -p'
+    if (gist_id) {
+        cmd = cmd + ' -u ' + gist_id;
+    }
+    cmd = cmd + ' $_nbname';
+    cmd = cmd + '\nprint lines[0].replace("https://gist.github.com", "").replace("/","")';
+    var callbacks = { 'iopub' : {'output' : IPython.ext_handle_gist_output}};
+    var x = IPython.notebook.kernel.execute(cmd, callbacks, {silent:false});
+    console.log('ran')
+    console.log(x)
+};
+
+setTimeout(function() {
+    if ($("#gist_notebook").length == 0) {
+        IPython.toolbar.add_buttons_group([
+            {
+                'label'   : 'Share Notebook as gist',
+                'icon'    : 'icon-share',
+                'callback': IPython.ext_gist_notebook,
+                'id'      : 'gist_notebook'
+            },
+        ])
+    }
+
+    if (IPython.notebook.metadata.gist_id) {
+        IPython.ext_update_gist_link(IPython.notebook.metadata.gist_id);
+    }
+}, 1000);
